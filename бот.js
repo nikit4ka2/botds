bbee0b60d5d442dbdcb8e1571c62b25c4630dc00
nikit4ka2 <52106910+nikit4ka2@users.noreply.github.com>
2019-06-22 18:15:50 +0300

    Create бот.js

---
+++ бот.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const fs = require('fs');
+bot.mutes = require('./mutes.json');
+let config = require('./botconfig.json');
+let token = config.token;
+let prefix = config.prefix;
+let profile = require('./profile.json');
+fs.readdir('./cmds/',(err,files)=>{
+    if(err) console.log(err);
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <=0) console.log("Нет комманд для загрузки!!");
+    console.log(`Загружено ${jsfiles.length} комманд`);
+    jsfiles.forEach((f,i) =>{
+        let props = require(`./cmds/${f}`);
+        console.log(`${i+1}.${f} Загружен!`);
+        bot.commands.set(props.help.name,props);
+    });
+});
+
+
+bot.on('ready', () => {
+    console.log(`Запустился бот ${bot.user.username}`);
+    bot.generateInvite(["ADMINISTRATOR"]).then(link =>{
+        console.log(link);
